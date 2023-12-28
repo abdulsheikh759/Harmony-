58c7967064e324d948139ecc9e5ac783d9716cfc
Abdul <abdulsheikh759@gmail.com>
2023-12-28 12:06:06 +0530

    change the code setup 5

---
+++ script.js
@@ -0,0 +1,14 @@
+// logi and sign up buuton
+
+const signn = document.getElementById('sign')
+
+signn.addEventListener("click",()=>{
+     window.open("http://127.0.0.1:5500/sign.html","_blank","width=500","height=500",)
+    });
+
+
+const login = document.getElementById('login');
+login.addEventListener("click",()=>{
+    window.open("http://127.0.0.1:5500/sign.html","_blank","width=500","height=500",) 
+})
+ 
